89b358dce1b94be0a7da100e51ad7446f6f1e8bc
stefan.avramovic <stefan.avramovic@academedia.se>
2021-12-29 15:24:23 +0100

    refactoring signout component

--- client/src/js/components/DashBoardComponent.js
+++ client/src/js/components/DashBoardComponent.js
@@ -1,7 +1,7 @@
 import NavBarContainer from '../container/NavBarContainer'
 import UsersListContainer from '../container/UsersListContainer'
 import React from 'react';
-function DashBoardComponent(props) { 
+function DashBoardComponent() { 
     return (
             <div className="dashboard bg_color">
                 <NavBarContainer />
